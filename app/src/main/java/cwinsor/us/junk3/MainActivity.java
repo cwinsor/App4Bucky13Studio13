5690b1359dd67af4fb5b775654541b2546e2c469
unknown <cdwinsor@FLA21-25.admin.wpi.edu>
2016-01-27 12:43:52 -0500

    first commit

---
+++ app/src/main/java/cwinsor/us/junk3/MainActivity.java
@@ -0,0 +1,83 @@
+package cwinsor.us.junk3;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // layout
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        relativeLayout.setBackgroundColor(Color.GREEN);
+
+        // button
+        Button button = new Button(this);
+        button.setText("Click Me !!!!!!");
+        button.setBackgroundColor((Color.RED));
+
+        // username input
+        EditText username = new EditText(this);
+
+        button.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Resources r = getResources();
+        int px = (int)TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                200,
+                r.getDisplayMetrics());
+
+        username.setWidth(px);
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        //
+        relativeLayout.addView(button, buttonDetails);
+        setContentView(relativeLayout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
